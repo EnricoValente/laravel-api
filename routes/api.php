9e66dbca32f043240458f6724c76da1f592da754
EnricoValente <enricovalente42@gmail.com>
2023-09-27 16:33:17 +0200

    creati controller e rotta api

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+ Route::resource('projects', ProjectController::class)->only([
+    'index',
+    'projects'
+ ]);
